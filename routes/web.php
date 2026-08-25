56013f16878958cb081ee2ea62ebe3a58096852b
Francesco Cassese <francescocassese9@gmail.com>
2026-08-25 20:27:57 +0200

    Add admin resource routes for projects

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +18,8 @@ Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('/', function () {
         return view('admin.dashboard');
     })->name('dashboard');
+
+    Route::resource('projects', ProjectController::class)->names('admin.projects');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
